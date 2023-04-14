b842fc75dd386e35f65f0661fc0781d1555f0b05
estebanstack <130319203+estebanstack@users.noreply.github.com>
2023-04-13 19:21:41 -0500

    Add files via upload

--- ejercicio.java
+++ ejercicio.java
@@ -1,9 +1,13 @@
 public class ejercicio{
-    public static void Ejercicio(String[] args) {
-
+    public static void main(String[] args) {
+            int result;
+            result= suma(45, 76, 89);
+            System.out.println(result);
     
     }
-
+    public static int suma(int a, int b, int c) {
+        return a+b+c;
+    }
 
 
 
